44b518fc89467af047dc0ed7e620473027dfcb17
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-04-11 14:45:35 -0300

    Threads icon added, refresh icon animation added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,38 @@
-import { StyleSheet } from "react-native"
+import { Platform, RefreshControl, ScrollView, StyleSheet } from "react-native"
 
 import EditScreenInfo from "@/components/EditScreenInfo"
 import { Text, View } from "@/components/Themed"
+import { SafeAreaView } from "react-native-safe-area-context"
+import Lottie from "lottie-react-native"
+import { useRef } from "react"
 
 export default function TabOneScreen() {
+	const animationRef = useRef<Lottie>(null)
+
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Diego</Text>
-			<View
-				style={styles.separator}
-				lightColor="#eee"
-				darkColor="rgba(255,255,255,0.1)"
-			/>
-			<EditScreenInfo path="app/(tabs)/index.tsx" />
-		</View>
+		<SafeAreaView>
+			<ScrollView
+				contentContainerStyle={{
+					paddingHorizontal: 10,
+				}}
+				refreshControl={
+					<RefreshControl
+						refreshing={false}
+						onRefresh={() => {
+							animationRef.current?.play()
+						}}
+						tintColor={"transparent"}
+					/>
+				}
+			>
+				<Lottie
+					ref={animationRef}
+					source={require("../../assets/images/threads.json")}
+					loop={false}
+					autoPlay
+					style={{ width: 90, height: 90, alignSelf: "center" }}
+				/>
+			</ScrollView>
+		</SafeAreaView>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: "bold",
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: "80%",
-	},
-})
